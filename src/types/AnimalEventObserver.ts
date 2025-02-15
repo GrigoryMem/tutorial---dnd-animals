55cf4d11a376570ca68ba07275977dc2ecf3161c
Grigory Morozov <morozehc@yandex.ru>
2025-02-15 14:46:03 +0300

    feat: add types for animalEventObsrver

--- src/types/AnimalEventObserver.ts
+++ src/types/AnimalEventObserver.ts
@@ -1,5 +1,15 @@
-export default interface AnimalEventObserver {
-  update(eventType: string, data?:any):void {
-
-  }
+export enum EAnimalEvents {
+  DRAG_START ='dragstart',
+  DRAG_END = 'dragend'
 }
+// наблюдатель типа Observer
+export default interface AnimalEventObserver {
+  update(eventType: string, data?:any):void 
+}
+
+// объект наблюдения за событиями животных
+export interface AnimalEventSubject {
+  subscribe(observer:AnimalEventObserver):void
+  unsubscribe(observer:AnimalEventObserver):void
+  notifyObservers(eventType: string, data?: any): void
+} 
