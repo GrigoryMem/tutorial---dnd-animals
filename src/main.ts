415bf9dbfdc155761a0795a394a0c91ef5848ad5
Grigory Morozov <morozehc@yandex.ru>
2025-02-21 23:05:19 +0300

    build(ci/cd): prepare project for build

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,17 @@ import ImageLoaderService from './services/imageLoaderService';
 import GameBuilder from './core/GameBuilder';
 import AudioService from './services/AudioService';
 import ConfettiService from './services/ConfettiService';
+const BASE_URL:string = import.meta.env.BASE_URL; 
 
-const audioService = new AudioService('/sound/')
+const audioService = new AudioService(`${BASE_URL}/sound/`)
 
 const gameBuilder:GameBuilder = new GameBuilder(
   new ImageLoaderService(),
   audioService, 
   dataAnimals);
 
-const game = await  gameBuilder
+;(async ()=>{
+  const game = await  gameBuilder
   .loadSounds(soundsData)
   .loadBackground(dataBackground)
   .loadImageAnimals()
@@ -20,32 +22,35 @@ const game = await  gameBuilder
 
 
   game.start()
-document.querySelector('#restart')?.addEventListener('click',(): void=>{
+  document.querySelector('#restart')?.addEventListener('click',(): void=>{
   game.restart()
-})
+  })
 
 
 // Выключение/включение звука 
 // ?????
-document.querySelector('#mute')?.addEventListener('click', (e: Event): void=>{
-  const isMute = audioService.toggleSound()
-  const button:HTMLButtonElement = e.target as HTMLButtonElement; //?
-  button.textContent = `Sound ${isMute ? 'Off' : 'On'}`;
-})
+  document.querySelector('#mute')?.addEventListener('click', (e: Event): void=>{
+    const isMute = audioService.toggleSound()
+    const button:HTMLButtonElement = e.target as HTMLButtonElement; //?
+    button.textContent = `Sound ${isMute ? 'Off' : 'On'}`;
+  })
 
-const confettiService: ConfettiService = new ConfettiService();
+  const confettiService: ConfettiService = new ConfettiService({});
 
 
 
-game.onEndGame(()=>{
-  // Создаем экземпляр ConfettiService, передаем настройки.
-// Запускаем конфетти на 10 секунд.
-// Через 5 секунд вручную останавливаем.
-  confettiService.start(5);
-  setTimeout(()=>{
-    alert('You win! Enjoy the game!')
-  },0)
-})
+  game.onEndGame(()=>{
+    // Создаем экземпляр ConfettiService, передаем настройки.
+  // Запускаем конфетти на 10 секунд.
+  // Через 5 секунд вручную останавливаем.
+    confettiService.start(5);
+    setTimeout(()=>{
+      alert('You win! Enjoy the game!')
+    },0)
+  })
+
+})()
+
 
 
   
